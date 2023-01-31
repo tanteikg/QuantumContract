9316ee50e3ef6b3e5d7af681a266b012993de4fc
Teik Guan Tan <tanteikg@gmail.com>
2023-01-31 23:34:00 +0800

    eod push

--- contracts/QuantumContract.sol
+++ contracts/QuantumContract.sol
@@ -22,29 +22,124 @@ contract QuantumContract
 
 	function getRandom(uint256 range) private view returns (uint) 
 	{
-		uint randomHash = uint(keccak256(block.difficulty, now));
+		uint randomHash = uint(keccak256(abi.encode(block.difficulty, block.timestamp)));
 		return randomHash % range;
 	}
 
-	function qc_H(uint256 mask, uint256 currState, int256[] memory Qubits) internal view
+	function qc_H(uint256 mask, uint256 currState, int256[] memory Qubits) internal pure 
 	{
-		Qubits[currState] += 1; 
+		if ((mask & currState) != 0)
+		{
+			Qubits[currState-mask] += Qubits[currState];
+			Qubits[currState] = 0 - Qubits[currState];
+		}				
+		else
+		{
+			Qubits[currState+mask] += Qubits[currState];
+			Qubits[currState] = Qubits[currState];
+		}
 
 	}
-	
-	function qc_exec(uint8 numQubits, byte[] memory nextGate, int256[] memory Qubits) internal view returns (uint8)
-	{
 
+	function qc_X(uint256 mask, uint256 currState, int256[] memory Qubits) internal pure 
+	{
+		if ((mask & currState) != 0)
+			Qubits[currState-mask] += Qubits[currState];
+		else
+			Qubits[currState+mask] += Qubits[currState];
+	}
+
+	function qc_I(uint256 mask, uint256 currState, int256[] memory Qubits) internal pure 
+	{
+		if ((mask & currState) != 0)
+			Qubits[currState] = Qubits[currState];
+		else
+			Qubits[currState] = Qubits[currState];
+	}
+
+	function qc_CN(uint256 cMask, uint256 mask, uint256 currState, int256[] memory Qubits) internal pure 
+	{
+		if (((cMask & currState) == cMask) && (cMask != 0))
+		{
+			if ((mask & currState) != 0)
+				Qubits[currState-mask] += Qubits[currState];
+			else
+				Qubits[currState+mask] += Qubits[currState];
+		}
+		else
+			Qubits[currState] = Qubits[currState];
+
+	}
+
+	function qc_exec(uint8 numQubits, bytes1[] memory qAlgo, int256[] memory Qubits) internal pure returns (uint8)
+	{
+		uint256 mask;
+		uint256 i;
+		uint256 j;
+		uint256 maxj=2^numQubits;
+
+		mask = 1;
+		mask <<= numQubits - 1;
+		for (i=0;i<numQubits;i++)
+		{
+			if (qAlgo[i] == GATE_H)
+			{
+				for(j=0;j<maxj;j++)
+				{
+					if (Qubits[j]!=0)
+						qc_H(mask,j,Qubits);
+				}
+			}
+			else if ((qAlgo[i] == GATE_I) || (qAlgo[i] == GATE_C))
+			{
+				for(j=0;j<maxj;j++)
+				{
+					if (Qubits[j]!=0)
+						qc_I(mask,j,Qubits);
+				}
+			}
+			else if (qAlgo[i] == GATE_X)
+			{
+				for(j=0;j<maxj;j++)
+				{
+					if (Qubits[j]!=0)
+						qc_X(mask,j,Qubits);
+				}
+			}
+			else if (qAlgo[i] == GATE_N)
+			{
+				uint256 tempVal = 1;
+				tempVal <<= numQubits-1;
+				uint256 cMask = 0;
+
+				for (j=0;i<numQubits;j++)
+				{
+					if (qAlgo[j] == GATE_C)
+						cMask += tempVal;
+					tempVal>>=1;
+				}
+				for(j=0;j<maxj;j++)
+				{
+					if (Qubits[j]!=0)
+						qc_CN(cMask,mask,j,Qubits);
+				}
+			}
+			else
+			{
+				revert("Unknown or unsupported gate");
+			}
+			mask >>=1;
+		}
+				
 		return numQubits;
 	}
 
 	function runQScript(uint8 numQubits, string memory s) public view returns (uint256) 
 	{
 		int256[] memory Qubits; 
-		byte[] memory nextGate;
+		bytes1[] memory nextGate;
 		uint256 ret;
 		bool done = false;
-		uint256 len;
 		uint256 i = 0;
 		uint256 j;
 		uint256 slen = bytes(s).length; 
@@ -53,7 +148,8 @@ contract QuantumContract
 			revert("MAX_QUBITS exceeded");	
 
 		Qubits = new int256[](2^numQubits);
-		nextGate = new byte[](numQubits);
+		Qubits[0] = 1; // start with all qubits = 0;
+		nextGate = new bytes1[](numQubits);
 		console.log("%s called runQScript with string length %d",msg.sender,bytes(s).length);
 		while (!done)
 		{
@@ -90,9 +186,9 @@ contract QuantumContract
 			i += uint(Qubits[j]);
 		j = getRandom(i);
 		ret = 0;
-		while (j > Qubit[ret])
+		while (j > uint(Qubits[ret]))
 		{
-			j -= Qubit[ret++];
+			j -= uint(Qubits[ret++]);
 		}	
 		return ret;
 			
---
+++ scripts/run.js
@@ -0,0 +1,29 @@
+const main = async () => 
+{
+	const [owner, randomPerson] = await hre.ethers.getSigners();
+	const quantumContractFactory = await hre.ethers.getContractFactory("QuantumContract");
+	const qContract = await quantumContractFactory.deploy();
+	await qContract.deployed();
+	console.log("Contract deployed to:", qContract.address);
+	console.log("Contract deployed by:",owner.address);
+
+	const qScript = await qContract.runQScript(1,"H.");
+	console.log("runQScript returned ",qScript);
+
+};
+
+const runMain = async () => 
+{
+	try 
+	{
+		await main();
+		process.exit(0); // exit Node process without error
+	} catch (error) 
+	{
+		console.log(error);
+		process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+	}
+  // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
+};
+
+runMain();
